2daae7cbdd7aa847d005c1239c44369fca2dee04
LAB201 <LAB201@serveduc.com>
2024-12-12 11:24:05 -0300

    Mudanca Boba

--- www/html/index.php
+++ www/html/index.php
@@ -4,4 +4,4 @@ include_once('../script/controller.php');
 $controller = new Controller();
 $controller->method = $_SERVER['REQUEST_METHOD'];
 $controller->params = $_GET;
-$controller();
+var_dump($controller());
