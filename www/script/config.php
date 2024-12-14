bc1c7294fdb0a109c8fa5868768052f98eaace6f
Loures <lucasantonio.loures@gmail.com>
2024-12-14 02:23:28 -0300

    Concertando o arquivo config

--- www/script/config.php
+++ www/script/config.php
@@ -2,11 +2,10 @@
 
 class Config
 {
-  const HOSTNAME = [
+  const HOST = [
     'Linux' => 'db_agro',
     'Windows' => 'localhost'
   ];
-  public $hostname = self::HOSTNAME[php_uname(mode: 's')];
   const USER = 'root';
   const PASSWORD = '';
   const DATABASE = 'agro';
