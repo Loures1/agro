e31f0a1ff74ad3a9cb02e429ecfc34bef8beeea4
LAB201 <LAB201@serveduc.com>
2025-02-03 08:34:56 -0300

    apagando funcoes

--- agro-mvc/app/classes/Uri.php
+++ agro-mvc/app/classes/Uri.php
@@ -8,8 +8,4 @@ class Uri
   {
     return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
   }
-
-  public function getController() {}
-  public function getMethod() {}
-  public function getParameter() {}
 }
