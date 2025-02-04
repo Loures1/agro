242906aa0fa9b714de955bd735b59eec682bf4a9
Loures <lucasantonio.loures@gmail.com>
2025-02-04 17:52:47 -0300

    feat: Consertando arquivo de configuracao

--- agro-mvc/config/CredentialsDB.php
+++ agro-mvc/config/CredentialsDB.php
@@ -3,12 +3,16 @@
 namespace config;
 
 class Credentials
-{ 
-    const USERNAME = null;
-    const PASSWORD = null;
-    const DATABASE = 'agro';
-    const HOSTNAME = match (php_uname(mode: 's')) {
-        'Linux' => 'db_agro',
-        'Windows' => 'localhost'
+{
+  const USERNAME = null;
+  const PASSWORD = null;
+  const DATABASE = 'agro';
+  public function hostname()
+  {
+    return match (php_uname(mode: 's')) {
+      'Linux' => 'db_agro',
+      'Windows NT' => 'localhost'
     };
+  }
 }
+
