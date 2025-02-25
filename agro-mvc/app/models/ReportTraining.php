52008298afc7d28fef29200a38b44bbee0e970a9
Loures <lucasantonio.loures@gmail.com>
2025-02-24 23:11:06 -0300

    feat: mudando a query da classe

--- agro-mvc/app/models/ReportTraining.php
+++ agro-mvc/app/models/ReportTraining.php
@@ -15,29 +15,13 @@ class ReportTraining extends Query
     $this->report = null;
     $this->idEmployed = self::getIdEmployed($matEmployed);
     $this->sqlCode =
-      "SELECT t.nome, ft.ativo
-      FROM tbl_treinamento AS t
-      INNER JOIN
-      (
-        SELECT c.id_treinamento,
-        CASE
-          WHEN ft.id_treinamento IS null THEN 0
-          ELSE 1
-        END ativo
-        FROM tbl_funcionario_treinamento AS ft
-        RIGHT JOIN
-        (
-          SELECT f.id AS id_funcionario, pt.id_treinamento
-          FROM tbl_funcionario AS f
-          CROSS JOIN tbl_profissao_treinamento AS pt
-          ON f.id_profissao = pt.id_profissao
-          WHERE f.id = {$this->idEmployed}
-        ) AS c
-        ON ft.id_treinamento = c.id_treinamento
-        AND ft.id_funcionario = c.id_funcionario
-      ) AS ft
-      ON t.id = ft.id_treinamento
-      ORDER BY ativo";
+      "SELECT
+      t.nome, ft.status, ft.data_vencimento
+      FROM tbl_funcionario_treinamento AS ft
+      RIGHT JOIN tbl_treinamento AS t
+      ON ft.id_treinamento = t.id
+      WHERE ft.id_funcionario = {$this->idEmployed}";
+
     $this->report = self::fetchReportTrainingInDataBase();
   }
 
