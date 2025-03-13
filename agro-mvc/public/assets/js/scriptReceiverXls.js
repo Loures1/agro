f0527599f12e3100a3dc0f94b8448288c1600926
chuvaemar <lowpolyheaven888@gmail.com>
2025-03-13 10:31:08 -0300

    feat: Css e js do arquivo receiverXls.html

---
+++ agro-mvc/public/assets/js/scriptReceiverXls.js
@@ -0,0 +1,5 @@
+
+document.getElementById('file-upload').addEventListener('change', function() {
+    const fileName = this.files[0]?.name || 'Nenhum arquivo escolhido';
+    document.querySelector('.file-name').textContent = fileName;
+});
