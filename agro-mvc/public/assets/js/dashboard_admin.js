200cb70af04ec2d27fc6c184fc227978f68a99b9
Loures <lucasantonio.loures@gmail.com>
2025-05-08 23:33:07 -0300

    feat: Fazendo uma parte muito chata

--- agro-mvc/public/assets/js/dashboard_admin.js
+++ agro-mvc/public/assets/js/dashboard_admin.js
@@ -1,10 +1,29 @@
 const switchs = Array.from(document.getElementsByClassName("switch"));
-const tables = Array.from(document.getElementsByClassName('table'));
+const tables = Array.from(document.getElementsByClassName("table"));
+const btn_poup = Array.from(document.getElementsByClassName("action"));
+const employeds_table = Array.from(
+  document.getElementsByClassName("employeds"),
+);
+const edit_employed = document.getElementById("edit_employed");
 
-switchs.forEach(element => {
-    element.addEventListener('click', function() {
-        tables.forEach(table => (table.id == element.id) 
-        ? table.style.display = 'block' 
-        : table.style.display = 'none');
-    });
+switchs.forEach((element) => {
+  element.addEventListener("click", function () {
+    tables.forEach((table) =>
+      table.id == element.id
+        ? (table.style.display = "block")
+        : (table.style.display = "none"),
+    );
+  });
+});
+
+btn_poup.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (btn.id == "edit") {
+      employeds_table.forEach((employeds_table) => {
+        if (employeds_table.contains(btn)) {
+          console.log(edit_employed.children);
+        }
+      });
+    }
+  });
 });
