a520ca666733e46a5e1c0cc631010f73df50f3fa
LAB201 <LAB201@serveduc.com>
2025-05-12 11:25:11 -0300

    feat: refatorando o coigo js admin

--- agro-mvc/public/assets/js/dashboard_admin.js
+++ agro-mvc/public/assets/js/dashboard_admin.js
@@ -2,9 +2,23 @@ const switchs = Array.from(document.getElementsByClassName("switch"));
 const tables = Array.from(document.getElementsByClassName("table"));
 const btn_poup = Array.from(document.getElementsByClassName("action"));
 const employeds_table = Array.from(
-  document.getElementsByClassName("employeds"),
+  document.getElementsByClassName("employeds")
 );
-const edit_employed = document.getElementById("edit_employed");
+const edit_employed = Array.from(
+  document.getElementById("edit_employed").children
+);
+
+let PoupEmployed = {
+  placeholders: function () {
+    return Array.from(
+      document.getElementById("edit_employed").children)
+      .filter(_ => _.id);  
+  },
+
+  placeholders_values: function(element) {
+    
+  }
+}
 
 switchs.forEach((element) => {
   element.addEventListener("click", function () {
@@ -19,9 +33,9 @@ switchs.forEach((element) => {
 btn_poup.forEach((btn) => {
   btn.addEventListener("click", function () {
     if (btn.id == "edit") {
-      employeds_table.forEach((employeds_table) => {
-        if (employeds_table.contains(btn)) {
-          console.log(edit_employed.children);
+      employeds_table.forEach((employed_table) => {
+        if (employed_table.contains(btn)) {
+          console.log(edit_employed);
         }
       });
     }
