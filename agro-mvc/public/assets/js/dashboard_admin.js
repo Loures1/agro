efe9fb493df3bebbddcf4a7afb2d51e87beffdd6
Loures <lucasantonio.loures@gmail.com>
2025-05-12 23:29:02 -0300

    feat: Feito o PopupEmployed

--- agro-mvc/public/assets/js/dashboard_admin.js
+++ agro-mvc/public/assets/js/dashboard_admin.js
@@ -1,24 +1,35 @@
 const switchs = Array.from(document.getElementsByClassName("switch"));
 const tables = Array.from(document.getElementsByClassName("table"));
 const btn_poup = Array.from(document.getElementsByClassName("action"));
-const employeds_table = Array.from(
-  document.getElementsByClassName("employeds")
-);
-const edit_employed = Array.from(
-  document.getElementById("edit_employed").children
-);
 
 let PoupEmployed = {
-  placeholders: function () {
-    return Array.from(
-      document.getElementById("edit_employed").children)
-      .filter(_ => _.id);  
+  set: function (btn) {
+    let placeholders = this.placeholders();
+    let placeholders_values = this.placeholders_values(btn);
+    placeholders.forEach((_) => {
+      if (placeholders_values.has(_.id)) {
+        _.id == "trainings"
+          ? (_.innerHTML = placeholders_values.get(_.id))
+          : (_.placeholder = placeholders_values.get(_.id));
+      }
+    });
   },
 
-  placeholders_values: function(element) {
-    
-  }
-}
+  placeholders: function () {
+    return Array.from(document.getElementById("edit_employed").children).filter(
+      (_) => _.id,
+    );
+  },
+
+  placeholders_values: function (btn) {
+    let tr_html = Array.from(document.getElementsByClassName("employeds"))
+      .filter((_) => _.contains(btn))
+      .shift();
+    return new Map(
+      Array.from(tr_html.children).map((_) => [_.id, _.innerHTML]),
+    );
+  },
+};
 
 switchs.forEach((element) => {
   element.addEventListener("click", function () {
@@ -33,11 +44,7 @@ switchs.forEach((element) => {
 btn_poup.forEach((btn) => {
   btn.addEventListener("click", function () {
     if (btn.id == "edit") {
-      employeds_table.forEach((employed_table) => {
-        if (employed_table.contains(btn)) {
-          console.log(edit_employed);
-        }
-      });
+      console.log(PoupEmployed.set(btn));
     }
   });
 });
