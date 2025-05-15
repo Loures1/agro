244325712dec969dcb845f0d42ceb226121fecab
Loures <lucasantonio.loures@gmail.com>
2025-05-14 22:06:57 -0300

    feat: Sei la

--- agro-mvc/public/assets/js/dashboard_admin.js
+++ agro-mvc/public/assets/js/dashboard_admin.js
@@ -1,6 +1,6 @@
 let Tables = {
   buttons: Array.from(document.getElementsByClassName("switch")),
-  
+
   tables: Array.from(document.getElementsByTagName("table")),
   set: function (button) {
     this.tables.map((_) =>
@@ -15,18 +15,16 @@ let PoupEdit = {
   buttons: Array.from(document.getElementsByClassName("action")),
 
   layout: function (button) {
-    return document.getElementById(button.value)
+    return document.getElementById(button.value);
   },
 
   set: function (button) {
-    var type_poup = Tables.tables
-    .filter((_) => _.contains(button))
-    .shift()
+    var type_poup = Tables.tables.filter((_) => _.contains(button)).shift();
     var poup = Array.from(
-      document.querySelector(`.${type_poup.className}.poup_edit`).children
-      .item(0)
-      .children
-      ).filter((_) => _.className);
+      document
+        .querySelector(`.${type_poup.className}.poup_edit`)
+        .children.item(0).children,
+    ).filter((_) => _.className);
     return poup;
   },
 };
@@ -39,7 +37,7 @@ Tables.buttons.forEach((button) => {
 
 PoupEdit.buttons.forEach((button) => {
   button.addEventListener("click", function () {
-    if (button.name == 'edit') {
+    if (button.name == "edit") {
       console.log(PoupEdit.set(button));
     }
   });
