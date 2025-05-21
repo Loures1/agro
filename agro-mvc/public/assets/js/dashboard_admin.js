827719be0542a6d4fa996090a8917e2ccae8735b
LAB201 <LAB201@serveduc.com>
2025-05-21 11:30:47 -0300

    Fazendo melhor

--- agro-mvc/public/assets/js/dashboard_admin.js
+++ agro-mvc/public/assets/js/dashboard_admin.js
@@ -1,89 +1,35 @@
-let dashboard = {
-  state: "employed",
-  scroll: "relative",
-  modal: null,
-  query: null
-}
+class Dashboard {
+  constructor() {
+    this.buttons = Array.from(document.getElementsByTagName('button'));
+  }
 
-let Tables = {
-  buttons: Array.from(document.getElementsByClassName("switch")),
+  listenButtons() {
+    const content = new Content();
+    
+  }
 
-  tables: Array.from(document.getElementsByTagName("table")),
-
-  set: function(button) {
-    this.tables.map((_) => {
-      if (_.className == button.value) {
+  changeTable(table) {
+    const content = new Content();
+    content.tables.map((_) => {
+      if (table == _.className) {
         _.style.display = "block"
-        dashboard.state = _.className
       } else {
         _.style.display = "none"
       }
-    }
-    );
-  },
-};
-
-let Popup = {
-  exit_buttons: Array.from(document.getElementsByClassName("exit")),
-
-  modals: Array.from(document.getElementsByTagName('dialog')),
-
-  register_buttons: Array.from(document.getElementsByClassName('action')),
-
-
-  set: function(button) {
-    let popup = document.getElementsByClassName(`${dashboard.state} popup_edit`)
-    popup = Array.from(popup[0].children)
-
-    let modal = this.modals.filter((_) => _.className == dashboard.state)
-      .shift()
-
-    let registers = document.getElementById(button.value)
-    registers = Array.from(registers.children)
-
-    popup.forEach((field) => {
-      registers.forEach((register) => {
-        if (field.className == register.className) {
-          field.className == 'trainings'
-            ? (field.innerHTML = register.innerHTML)
-            : (field.placeholder = register.innerText)
-        }
-      })
     })
-    modal.showModal()
-    document.body.style.position = "fixed"
-
-    dashboard.modal = modal;
-    dashboard.scroll = "fixed"
   }
 }
 
-Tables.buttons.forEach((button) => {
-  button.addEventListener("click", function() {
-    Tables.set(button);
-  });
-});
-
-Popup.register_buttons.forEach((button) => {
-  button.addEventListener("click", function() {
-    if (button.name == 'edit') {
-      Popup.set(button)
-    }
-  });
-})
-
-Popup.exit_buttons.forEach((button) => {
-  button.addEventListener("click", function() {
-    dashboard.modal.close()
-    document.body.style.position = "relative"
-    dashboard.scroll = "relative"
-  })
-})
-
-document.addEventListener("keydown", function(event) {
-  if (event.key == "Escape" && dashboard.scroll == "fixed") {
-    document.body.style.position = "relative"
-    dashboard.scroll = "relative"
+class Content {
+  constructor() {
+    this.tables = Array.from(document.getElementsByTagName('table'))
   }
-})
 
+  getTable(target) {
+    return this.tables.filter((table) => table.className == target).shift()
+  }
+
+}
+
+const dash = new Dashboard()
+dash.listenButtons()
