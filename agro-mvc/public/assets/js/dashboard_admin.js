ce386765267dfcea52c7740530f705e94b7e5bac
Loures <lucasantonio.loures@gmail.com>
2025-05-26 23:41:24 -0300

    feat: Fazendo Dashboard

--- agro-mvc/public/assets/js/dashboard_admin.js
+++ agro-mvc/public/assets/js/dashboard_admin.js
@@ -1,35 +1,59 @@
-class Dashboard {
-  constructor() {
-    this.buttons = Array.from(document.getElementsByTagName('button'));
-  }
+const Dashboard = {
+  status: "employed",
 
-  listenButtons() {
-    const content = new Content();
-    
-  }
+  buttons: Array.from(document.querySelectorAll("button")),
 
-  changeTable(table) {
-    const content = new Content();
-    content.tables.map((_) => {
-      if (table == _.className) {
-        _.style.display = "block"
+  listener: function() {
+    this.buttons.forEach((button) => {
+      button.addEventListener("click", function() {
+        switch (button.className) {
+          case "switch":
+            Dashboard.status = Dashboard.defineTable(button.value)
+            break
+          case "edit":
+            Dashboard.mountPoupEdit(button.value)
+            break
+        }
+      })
+    })
+  },
+
+  defineTable: function(target) {
+    Content.tables.forEach((table) => {
+      if (table.className == target) {
+        table.style.display = "block"
       } else {
-        _.style.display = "none"
+        table.style.display = "none"
+      }
+    })
+    return target
+  },
+
+  mountPoupEdit: function(target) {
+    let register = Content.getRegister(target)
+    register = register.filter((field) => field.className)
+    register.forEach((field) => {
+      switch (field.firstChild.nodeName) {
+        case "#text":
+          Content.poup_edit.innerHTML = ``
+          break
       }
     })
   }
 }
 
-class Content {
-  constructor() {
-    this.tables = Array.from(document.getElementsByTagName('table'))
-  }
+const Content = {
+  tables: Array.from(document.querySelectorAll("table")),
 
-  getTable(target) {
-    return this.tables.filter((table) => table.className == target).shift()
-  }
+  poup_edit: document.querySelectorAll("dialog > div.content").item(0),
 
+  getRegister: function(target) {
+    return Array.from(
+      document.querySelectorAll(`#${target}`)
+        .item(0)
+        .children
+    )
+  }
 }
 
-const dash = new Dashboard()
-dash.listenButtons()
+Dashboard.listener()
