4163911df78acd4eb2b5fb8097e814eec3c4b166
LAB201 <LAB201@serveduc.com>
2025-05-27 11:25:01 -0300

    feat: Design surginto

--- agro-mvc/public/assets/js/dashboard_admin.js
+++ agro-mvc/public/assets/js/dashboard_admin.js
@@ -11,7 +11,8 @@ const Dashboard = {
             Dashboard.status = Dashboard.defineTable(button.value)
             break
           case "edit":
-            Dashboard.mountPoupEdit(button.value)
+            Dashboard.mountPoup(button.value)
+            Popup.show()
             break
         }
       })
@@ -29,13 +30,17 @@ const Dashboard = {
     return target
   },
 
-  mountPoupEdit: function(target) {
+  mountPoup: function(target) {
+    Content.poup.innerHTML = null
     let register = Content.getRegister(target)
     register = register.filter((field) => field.className)
     register.forEach((field) => {
       switch (field.firstChild.nodeName) {
         case "#text":
-          Content.poup_edit.innerHTML = ``
+          Content.poup.innerHTML += Popup.mountText(field)
+          break
+        case "UL":
+          Content.poup.innerHTML += Popup.mountSelect(field)
           break
       }
     })
@@ -45,7 +50,7 @@ const Dashboard = {
 const Content = {
   tables: Array.from(document.querySelectorAll("table")),
 
-  poup_edit: document.querySelectorAll("dialog > div.content").item(0),
+  poup: document.querySelectorAll("dialog > div.content").item(0),
 
   getRegister: function(target) {
     return Array.from(
@@ -53,6 +58,46 @@ const Content = {
         .item(0)
         .children
     )
+  }
+}
+
+const Popup = {
+  show: function() {
+    document.querySelector("dialog").showModal()
+  },
+
+  mountText: function(field) {
+    let label_title = field.className
+    let placeholder = field.innerHTML
+    let text = `
+      <label>
+        ${label_title}:
+        <input type="text" placeholder="${placeholder}">
+      </label><br>`
+    return text
+  },
+
+  mountSelect: function(field) {
+    let table =  Content.tables
+    table = table.filter((table) => table.className == field.className).shift()
+    table = Array.from(table.querySelectorAll("tr"))
+    table = table.filter((register) => register.id)
+    var option = ""
+    table.forEach((register) => {
+      option += `
+        <option value="${register.id}">
+          ${register.querySelector(".name").innerHTML}
+        </option>`
+    })
+    let select = `<select>${option}</select>`
+    let list_items = Array.from(field.querySelectorAll('li'))
+    let string_items = ""
+    list_items.forEach((item) => {
+      string_items += `<li>${item.innerHTML}<button>X</button></li>`
+    })
+    string_items = `<ul>${string_items}</ul>`
+    let content = `<div>${string_items}${select}</div>`
+    return content
   }
 }
 
